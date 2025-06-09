c974bb98f9e7095572e30febaf8c783993189adb
SamuelWaridel <114981092+SamuelWaridel@users.noreply.github.com>
2025-06-09 14:39:38 +0200

    changes to black box attack

--- Functions/implementations.py
+++ Functions/implementations.py
@@ -453,7 +453,9 @@ def attack_model(model, device, batch_size=1):
 
     # Load images from CIFAR-10
     transform = transforms.Compose([
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
     ])
 
     dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
